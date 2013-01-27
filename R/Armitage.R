cdf998517a5224e493a331fd10c710a5ab18226e
Yongshuai Jiang <jiangyongshuai@gmail.com>
2013-01-27 00:00:00 +0000

    version 1.1.3

--- R/Armitage.R
+++ R/Armitage.R
@@ -1,18 +1,39 @@
 Armitage <-
-function(genotypeStat){
-     if(ncol(genotypeStat)==3 && nrow(genotypeStat)==2){
-	     sum12=sum(genotypeStat)
-         sum1=sum(genotypeStat[1,]) 
-         sum2=sum(genotypeStat[2,])
-         temp1=2*genotypeStat[1,1]+genotypeStat[1,2]
-         temp2=2*genotypeStat[1,1]+genotypeStat[1,2]+2*genotypeStat[2,1]+genotypeStat[2,2]  
-         temp3=4*genotypeStat[1,1]+genotypeStat[1,2]+4*genotypeStat[2,1]+genotypeStat[2,2]
-         upp=sum12*(sum12*temp1-sum1*temp2)*(sum12*temp1-sum1*temp2)
-         low=sum1*sum2*(sum12*temp3-temp2*temp2)
-         statistic=upp/low
-         pValue=pchisq(statistic,df=1,lower.tail=FALSE)
-         list(statistic=statistic,pValue=pValue)
-	 }else{
-	     list(statistic=NA,pValue=NA)
-	 }
+function(case_11,case_12,case_22,control_11,control_12,control_22)
+{
+     # if ( control_11<0 || control_11 != round(control_11)) {
+         # stop("'control_11' must be a positive integer.")
+     # }
+	 # if ( control_12<0 || control_12 != round(control_12)) {
+         # stop("'control_12' must be a positive integer.")
+     # }
+	 # if ( control_22<0 || control_22 != round(control_22)) {
+         # stop("'control_22' must be a positive integer.")
+     # }
+	
+	 # if ( case_11<0 || case_11 != round(case_11)) {
+         # stop("'case_11' must be a positive integer.")
+     # }
+	 # if ( case_12<0 || case_12 != round(case_12)) {
+         # stop("'case_12' must be a positive integer.")
+     # }
+ 	 # if ( case_22<0 || case_22 != round(case_22)) {
+         # stop("'case_22' must be a positive integer.")
+     # }
+	 
+	 # Armitage¡¯s trend test for the 2x3 genotype table
+     N = case_11+case_12+case_22+control_11+control_12+control_22
+     R = case_11+case_12+case_22
+     S = N-R
+     temp1 = case_12 + 2 * case_22
+     n1=case_12+control_12
+	 n2=case_22+control_22
+	 temp2 = n1+2*n2
+	 temp3 = n1+4*n2
+	 upp=N*(N*temp1-R*temp2)*(N*temp1-R*temp2)
+	 low=S*R*(N*temp3-temp2*temp2)
+
+     statistic = upp/low
+     pValue = pchisq(statistic, df = 1, lower.tail = FALSE)
+     return(list(statistic = statistic, pValue = pValue))
 }
